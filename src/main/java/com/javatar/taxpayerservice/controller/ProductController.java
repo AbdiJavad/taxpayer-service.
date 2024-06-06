7efc52897afb2f192aa0f3a62982d8d7c9a83b1b
JavadAbdi <Mirjavadabdi@gmail.com>
2024-06-07 00:27:52 +0330

    Updated,Changed

--- src/main/java/com/javatar/taxpayerservice/controller/ProductController.java
+++ src/main/java/com/javatar/taxpayerservice/controller/ProductController.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class ProductController {
     @Autowired
     private ProductService productService;
+
     @PostMapping
     public Product save(
             @RequestBody Product product) {
