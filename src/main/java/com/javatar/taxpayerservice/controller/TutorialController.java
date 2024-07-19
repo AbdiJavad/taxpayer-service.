507e73beaffd8ace67475272d0e30966abd0b5a0
JavadAbdi <Mirjavadabdi@gmail.com>
2024-07-19 23:42:17 +0330

    Updated,Changed

--- src/main/java/com/javatar/taxpayerservice/controller/TutorialController.java
+++ src/main/java/com/javatar/taxpayerservice/controller/TutorialController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
+
     
     @Autowired
     TutorialRepository tutorialRepository;
