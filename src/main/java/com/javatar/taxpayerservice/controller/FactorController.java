f68eadbd2de1b23ec91d090a638685caf3c73931
JavadAbdi <Mirjavadabdi@gmail.com>
2024-07-23 00:08:12 +0330

    Updated,Changed

--- src/main/java/com/javatar/taxpayerservice/controller/FactorController.java
+++ src/main/java/com/javatar/taxpayerservice/controller/FactorController.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class FactorController {
     @Autowired
     private FactorService factorService;
-    
+
     @PostMapping("/{customerid}/{sellerid}")
     public Factor save(
             @PathVariable(value = "customerid") Long customerid,
