553a904444fbbba387ba4570d397249853751909
JavadAbdi <Mirjavadabdi@gmail.com>
2024-09-22 00:53:22 +0330

    Updated,Changed

--- src/main/java/com/javatar/taxpayerservice/controller/TutorialDetailsController.java
+++ src/main/java/com/javatar/taxpayerservice/controller/TutorialDetailsController.java
@@ -19,6 +19,7 @@ public class TutorialDetailsController {
     @Autowired
     private TutorialRepository tutorialRepository;
 
+    
     @PostMapping("/tutorials/{tutorialId}/details")
     public ResponseEntity<TutorialDetails> createDetails(@PathVariable(value = "tutorialId") Long tutorialId,
                                                          @RequestBody TutorialDetails detailsRequest) {
