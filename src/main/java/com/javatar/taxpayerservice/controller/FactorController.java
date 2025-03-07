4266e79723a3d39d75f368a266b695903877a505
JavadAbdi <Mirjavadabdi@gmail.com>
2025-03-07 21:38:13 +0330

    Updated,Changed

--- src/main/java/com/javatar/taxpayerservice/controller/FactorController.java
+++ src/main/java/com/javatar/taxpayerservice/controller/FactorController.java
@@ -1,7 +1,6 @@
 package com.javatar.taxpayerservice.controller;
 
 
-
 import com.javatar.taxpayerservice.domain.Factor;
 import com.javatar.taxpayerservice.service.FactorService;
 import io.swagger.v3.oas.annotations.tags.Tag;
