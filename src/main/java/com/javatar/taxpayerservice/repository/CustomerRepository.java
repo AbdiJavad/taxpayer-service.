704f8a8b622f79d9df2bb8d5ff9b25609fb10121
JavadAbdi <Mirjavadabdi@gmail.com>
2025-05-05 18:07:30 +0330

    Updatedm,Changed

--- src/main/java/com/javatar/taxpayerservice/repository/CustomerRepository.java
+++ src/main/java/com/javatar/taxpayerservice/repository/CustomerRepository.java
@@ -1,6 +1,5 @@
 package com.javatar.taxpayerservice.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.javatar.taxpayerservice.domain.Customer;
