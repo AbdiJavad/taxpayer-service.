1ba1f5c1a80dc045b9c3b1c3fa225f929f6d5343
JavadAbdi <Mirjavadabdi@gmail.com>
2025-07-24 15:53:08 +0330

    Updated,Changed

--- src/main/java/com/javatar/taxpayerservice/repository/SellerRepository.java
+++ src/main/java/com/javatar/taxpayerservice/repository/SellerRepository.java
@@ -1,7 +1,6 @@
 package com.javatar.taxpayerservice.repository;
 
 
-
 import com.javatar.taxpayerservice.domain.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
